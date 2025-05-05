4ca88e6f4434bf2eda0ccf012eb6e0bc64b3b13d
lamtota40 <lamtota40@gmail.com>
2025-05-06 06:21:18 +0700

    Update auto-run-grml.sh

--- auto-run-grml.sh
+++ auto-run-grml.sh
@@ -7,4 +7,6 @@ mount /dev/vda2 /mnt/vda2
 echo -n > /mnt/vda2/etc/overlayroot.conf
 echo 'overlayroot_cfgdisk="disabled"' > /mnt/vda2/etc/overlayroot.conf
 echo 'overlayroot=""' > /mnt/vda2/etc/overlayroot.conf
+echo 'overlayroot=""' > /mnt/vda2/etc/overlayroot.conf
+tmpfs
 reboot
